e748f617d8d5ac56c74c9dd1570a17c44e4eaa8c
yuval <yuvedavid@gmail.com>
2023-12-01 19:46:46 +0200

    added

---
+++ src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import './Home.css';
+
+export default function Home() {
+    return (
+        <>
+            <div className="homePage">
+                <div className="bigDiv">מסגרת גדולה</div>
+                <div className="smallDiv">מסגרת קטנה</div>
+            </div>
+        </>
+    )
+}
